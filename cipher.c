c607154122ed84f61720be0e3502c41bc2ef67ca
Craig Garrigan <cg462@st-andrews.ac.uk>
2011-06-29 19:27:21 +0100

    Encipher and decipher prototypes added to cipher.h index variable removed from cipher.h main() fleshed out

--- cipher.c
+++ cipher.c
@@ -2,5 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
+	printf("encipher('hello world') => %s\n", encipher("hello world'));
+	printf("decipher(encipher('hello world')) => %s\n", decipher(encipher("hello world")));
 	return 0;
 }
--- cipher.h
+++ cipher.h
@@ -2,6 +2,11 @@
 #define CIPHER_H
 
 char *cipher = {'p', 'l', 'm', 'o', 'k', 'n', 'i', 'j', 'b', 'u', 'h', 'v', 'y', 'g', 'c', 't', 'f', 'x', 'r', 'd', 'z', 'e', 's', 'w', 'a', 'q'};
-int index = 0;
+
+/* Enciphers the plaintext using the cipher key and returns the ciphertext */
+char *encipher(const char *plaintext);
+
+/* Deciphers the ciphertext using the cipher key and returns the plaintext */
+char *decipher(const char *ciphertext);
 
 #endif /* CIPHER_H */
